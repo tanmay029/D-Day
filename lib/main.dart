0d01351daaab836b2e0c337ba9e567d41ade84ee
maxp0290 <tanmaykumar1202@gmail.com>
2025-02-18 03:42:01 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:dooms_day/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'D-Day',
+      initialRoute: "/home",
+      routes: {
+        '/home': (_) => HomePage(),
+      },
+    );
+  }
+}
