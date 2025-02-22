264726e0e6999aedc12b9ca0d4c5ce1110847a63
maxp0290 <tanmaykumar1202@gmail.com>
2025-02-23 00:30:36 +0530

    step tracker page completed

--- lib/pages/health.dart
+++ lib/pages/health.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 
 class StepTrackerScreen extends StatefulWidget {
   @override
@@ -11,11 +14,14 @@ class _StepTrackerScreenState extends State<StepTrackerScreen> {
   int currentSteps = 0;
   int goalSteps = 10000;
   late Stream<StepCount> _stepCountStream;
+  String lastRecordedDate = "";
 
   @override
   void initState() {
     super.initState();
+    _loadPreferences();
     _initPedometer();
+    _requestBackgroundPermission();
   }
 
   void _initPedometer() {
@@ -24,37 +30,128 @@ class _StepTrackerScreenState extends State<StepTrackerScreen> {
   }
 
   void _onStepCount(StepCount event) {
-    setState(() {
-      currentSteps = event.steps; // Update the step count
-    });
+    String today = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    if (lastRecordedDate != today) {
+      setState(() {
+        currentSteps = 0;
+        goalSteps = 0;
+        lastRecordedDate = today;
+      });
+      _savePreferences();
+    } else {
+      setState(() {
+        currentSteps = event.steps;
+      });
+    }
   }
 
   void _onStepError(error) {
     print("Pedometer error: $error");
   }
 
+  Future<void> _loadPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      goalSteps = prefs.getInt('goalSteps') ?? 10000;
+      lastRecordedDate = prefs.getString('lastRecordedDate') ?? "";
+      currentSteps = prefs.getInt('currentSteps') ?? 0;
+    });
+  }
+
+  Future<void> _savePreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('goalSteps', goalSteps);
+    await prefs.setString('lastRecordedDate', lastRecordedDate);
+    await prefs.setInt('currentSteps', currentSteps);
+  }
+
+  Future<void> _requestBackgroundPermission() async {
+    print("Requesting background activity permission...");
+  }
+
+  void _showGoalInputDialog() {
+    TextEditingController goalController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Set Step Goal"),
+          content: TextField(
+            controller: goalController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: "Enter step goal"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                int? newGoal = int.tryParse(goalController.text);
+                if (newGoal != null && newGoal > 0) {
+                  setState(() {
+                    goalSteps = newGoal;
+                  });
+                  _savePreferences();
+                }
+                Navigator.pop(context);
+              },
+              child: Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _resetSteps() {
+    setState(() {
+      currentSteps = 0;
+      goalSteps = 0;
+    });
+    _savePreferences();
+  }
+
   @override
   Widget build(BuildContext context) {
-    double percentage = (currentSteps / goalSteps).clamp(0.0, 1.0);
-    
+    double percentage =
+        goalSteps > 0 ? (currentSteps / goalSteps).clamp(0.0, 1.0) : 0.0;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Beta Testing"),
+        title: Text("Step Tracker"),
+        backgroundColor: Colors.orangeAccent,
       ),
-      backgroundColor: Color(0xFFF2A900),
+      backgroundColor: Colors.orange.shade100,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StepProgressIndicator(currentSteps: currentSteps, percentage: percentage),
+            Text(
+              "Setup Daily Walking Goal",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            StepProgressIndicator(
+                currentSteps: currentSteps,
+                goalSteps: goalSteps,
+                percentage: percentage),
             SizedBox(height: 20),
-            GoalSelectionWidget(
-              goalSteps: goalSteps,
-              onGoalChanged: (newGoal) {
-                setState(() {
-                  goalSteps = newGoal;
-                });
-              },
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _showGoalInputDialog,
+                  child: Text("Setup Steps"),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _resetSteps,
+                  child: Text("Reset"),
+                ),
+              ],
             ),
           ],
         ),
@@ -65,78 +162,68 @@ class _StepTrackerScreenState extends State<StepTrackerScreen> {
 
 class StepProgressIndicator extends StatelessWidget {
   final int currentSteps;
+  final int goalSteps;
   final double percentage;
 
-  StepProgressIndicator({required this.currentSteps, required this.percentage});
+  StepProgressIndicator(
+      {required this.currentSteps,
+      required this.goalSteps,
+      required this.percentage});
 
-  @override
-  Widget build(BuildContext context) {
-    return CircularPercentIndicator(
-      radius: 120.0,
-      lineWidth: 10.0,
-      percent: percentage,
-      center: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "$currentSteps",
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          Text(
-            "STEPS",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      progressColor: Colors.white,
-      backgroundColor: Colors.white.withOpacity(0.3),
-      circularStrokeCap: CircularStrokeCap.round,
-    );
+  String getMotivationalText() {
+    if (percentage <= 0.25) {
+      return "You are better than this, You can do it !!";
+    } else if (percentage <= 0.49) {
+      return "Don't give up you are almost there !!";
+    } else if (percentage <= 0.99) {
+      return "Most people give up right before they reach their goal, don't add to that count!!";
+    } else {
+      return "You did it, you are better than 70% of the people!!!";
+    }
   }
-}
-
-class GoalSelectionWidget extends StatelessWidget {
-  final int goalSteps;
-  final ValueChanged<int> onGoalChanged;
-
-  GoalSelectionWidget({required this.goalSteps, required this.onGoalChanged});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        CircularPercentIndicator(
+          radius: 120.0,
+          lineWidth: 10.0,
+          percent: percentage,
+          center: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$currentSteps / $goalSteps",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                "STEPS",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          progressColor: Colors.orange,
+          backgroundColor: Colors.white.withOpacity(0.3),
+          circularStrokeCap: CircularStrokeCap.round,
+        ),
+        SizedBox(height: 20),
         Text(
-          "GOAL: $goalSteps STEPS",
+          getMotivationalText(),
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 16,
             fontWeight: FontWeight.bold,
             color: Colors.black,
           ),
-        ),
-        Slider(
-          min: 1000,
-          max: 20000,
-          divisions: 19,
-          value: goalSteps.toDouble(),
-          onChanged: (value) {
-            onGoalChanged(value.toInt());
-          },
-        ),
-        Text(
-          "164 CAL",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
+          textAlign: TextAlign.center,
         ),
       ],
     );
